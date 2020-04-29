86b621266daf98e624cb2e8a724e496796401794
tockn <s.taku.inthebox@gmail.com>
2020-04-29 18:05:36 +0900

    separate websocket

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
 
 type SDP string
 
 type Room struct {
 	ID      string
 	Name    string
-	Clients []*Client
+	Clients map[string]*Client
 }
 
 func CreateRoom(name string) (*Room, error) {
 	return nil, nil
+}
+
+func Join(name string) error {
+	return nil
 }
 
 func TransferSDPOffer(sdp *SDP, senderClientID string) error {
@@ -25,16 +34,72 @@ func TransferSDPAnswer(sdp *SDP, senderClientID string) error {
 type Client struct {
 	ID   string
 	Name string
+	Send chan []byte
 }
 
-func (c *Client) JoinRoom(roomID string) (*Room, error) {
-	return nil, nil
+// ListenMessage should be executed in goroutine
+func (c *Client) ListenMessage() {
 }
 
 type Handler struct{}
 
-func (h *Handler) CreateConnection(w http.ResponseWriter, r *http.Request) {}
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
+// CreateConnectionでwebsocketコネクションを確立
+// Clientを作成してレスポンスとして通知する
+func (h *Handler) CreateConnection(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	c := &Client{}
+	go h.HandleSendMessage(c, conn)
+}
+
+func (h *Handler) HandleSendMessage(c *Client, conn *websocket.Conn) {
+	defer func() {
+		_ = conn.Close()
+	}()
+	for {
+		select {
+		case msg := <-c.Send:
+			w, err := conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(msg)
+			if err := w.Close(); err != nil {
+				return
+			}
+		}
+	}
+}
+
+// HandleMessageはWebsocketで来たメッセージのTypeを元に適切なHandle関数を実行する
+func (h *Handler) HandleReceiveMessage(conn *websocket.Conn) {
+	// Typeを元に、SDPOffer, SDPAnswerを実行する
+	// 返り値がnilでなければ、そのResponseMessageを送信する
+	defer func() {
+		_ = conn.Close()
+	}()
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			return
+		}
+		// Unmarshalして適切なmethod実行
+	}
+}
+
+type RequestSDPOffer struct {
+	RoomID string `json:"room_id"`
+	SDP    SDP    `json:"sdp"`
+}
+
+// SDP offerが来た時に呼ばれる。Room IDの
 func (h *Handler) SDPOffer(msg *RequestMessage) *ResponseMessage {
 	return nil
 }
@@ -43,7 +108,10 @@ func (h *Handler) SDPAnswer(msg *RequestMessage) *ResponseMessage {
 	return nil
 }
 
-type RequestMessage struct{}
+type RequestMessage struct {
+	Type    string          `json:"type"`
+	Payload json.RawMessage `json:"payload"`
+}
 type ResponseMessage struct{}
 
 func main() {
